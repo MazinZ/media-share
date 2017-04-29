921953a290c421ed19be32afc99a0a0d5cea4e85
Mazin Zakaria <mazinzakaria1@gmail.com>
2017-04-29 01:47:52 -0400

    Forgot to add NavbarController

---
+++ angular/app/js/controllers/navbar_controller.js
@@ -0,0 +1,23 @@
+angular.module(app_name).controller('NavbarController', ['$scope', '$rootScope', '$timeout', '$window', 'user_service',
+  function ($scope, $rootScope, $timeout, $window, user_service) {
+
+  if (!user_service.user && user_service.is_signed_in()) {
+    user_service.set_user();
+  }
+
+  $rootScope.$on('USER_SET', function(event, user){
+    init(user);
+  });
+
+  $rootScope.$on('USER_CLEARED', function(event, user){
+    $scope.username = undefined;
+  });
+
+  function init() {
+    $timeout(function() {
+      $scope.user = user_service.user;
+      $scope.username = $scope.user.username;
+    });
+  }
+
+}]);
